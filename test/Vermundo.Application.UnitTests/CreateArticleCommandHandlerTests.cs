ebe02028cd33401fb94e72aa941f6ab2eae8bf1d
dimitri <andre.d3003@gmail.com>
2025-07-09 21:53:19 +0200

    CreateArticleCommandHandlerTests

---
+++ test/Vermundo.Application.UnitTests/CreateArticleCommandHandlerTests.cs
@@ -0,0 +1,29 @@
+﻿using Moq;
+namespace Vermundo.Application.UnitTests;
+
+public class CreateArticleCommandHandlerTests
+{
+    [Fact]
+    public async Task Handle_GivenValidCommand_ShouldSaveArticleToRepository()
+    {
+        // Arrange
+        var mockRepo = new Mock<IArticleRepository>();
+        var handler = new CreateArticleCommandHandler(mockRepo.Object);
+
+        var command = new CreateArticleCommand
+        {
+            Title = "Test Title",
+            Body = "First paragraph.\n\nSecond paragraph."
+        };
+
+        // Act
+        await handler.Handle(command, CancellationToken.None);
+
+        // Assert
+        mockRepo.Verify(r =>
+            r.AddAsync(It.Is<Article>(a =>
+                a.Title == command.Title &&
+                a.Body == command.Body)),
+            Times.Once);
+    }
+}
