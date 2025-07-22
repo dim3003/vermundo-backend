1cddec4d2dd019de341634055953f16f9318f51c
Dimitri Andre <dimitri@bonapp.group>
2025-07-22 15:17:01 +0200

    createarticle endpoint red test

---
+++ test/Vermundo.Api.FunctionalTests/Articles/CreateArticleTests.cs
@@ -0,0 +1,28 @@
+﻿using Bogus;
+using System.Net;
+using System.Net.Http.Json;
+using Vermundo.Api.Controllers.Articles;
+using Vermundo.Api.FunctionalTests.Infrastructure;
+
+namespace Vermundo.Api.FunctionalTests.Articles;
+
+public class CreateArticleTests : BaseFunctionalTests
+{
+    private readonly Faker _faker = new();
+
+    public CreateArticleTests(FunctionalTestsWebAppFactory factory) : base(factory)
+    {
+    }
+
+    [Fact]
+    public async Task CreateArticle_ShouldReturnIsSuccess_WhenValid()
+    {
+        var request = new CreateArticleRequest(_faker.Lorem.Sentence(), _faker.Lorem.Paragraph());
+
+        var response = await HttpClient.PostAsJsonAsync("api/articles",
+            request
+        );
+
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+}
